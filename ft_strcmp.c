2a4b9aa970c2eda3b3d721db1371154e4cf01256
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 14:58:27 +0200

    Update ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -7,3 +7,13 @@ int	ft_strcmp(char *s1, char *s2)
 	}
 	return (*s1 - *s2);
 }
+
+#include <stdio.h>
+int main ()
+ {
+     char s1[] = "meggyozo";
+     char s2[] = "meggyoz";
+     
+     printf("%d", ft_strcmp(s1, s2));
+     return (0);
+ }
