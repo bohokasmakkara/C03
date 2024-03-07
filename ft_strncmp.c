9560b97a1225f223d6083ad68f4fc29501018eda
bohokasmakkara <162553789+bohokasmakkara@users.noreply.github.com>
2024-03-07 14:58:58 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -12,3 +12,12 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	}
 	return (0);
 }
+#include <stdio.h>
+int main()
+{
+    char s1[] = "meggyozo";
+    char s2[] = "megnez";
+
+    printf("%d", ft_strncmp(s1, s2, 4)); // Compare only the first 4 characters
+    return 0;
+}
